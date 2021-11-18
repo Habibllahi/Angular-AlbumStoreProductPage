1921f8b5059ebc82bebc1738506cfae448b9d3a8
Hamzat Habibllahi Adewale <habibllahi3@gmail.com>
2021-11-18 15:39:53 +0100

    test 2 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http'
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ProductDescriptionComponent,
     ProductPageComponent
   ],
   imports: [
